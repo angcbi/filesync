6949c245d5ec509652ef42c10c8a612365b0b682
cc@192.168.1.151 <cc@192.168.1.151>
2016-10-27 19:29:57 +0800

    1

--- filesender.py
+++ filesender.py
@@ -2,6 +2,7 @@
 # encoding: utf-8
 
 import os
+import json
 import pickle
 import hashlib
 
@@ -10,8 +11,9 @@ def createmd5(path):
       templist = []
       config = os.path.join(path, 'filesender.ini')
       try:
-            confighandler = open(config, 'wb+')
-            templist = pickle.load(confighandler)
+            if os.path.exists(config):
+                  with open(config, 'rb') as confighandler:
+                        templist = pickle.load(confighandler)
             for file in os.listdir(path):
                   file = os.path.join(path, file)
                   if os.path.isfile(file):
@@ -20,34 +22,33 @@ def createmd5(path):
                         with open(file, 'rb') as f:
                               for data in f:
                                     t.update(data)
-                        tempdict['filename'] = file
-                        tempdict['md5'] = t.hexdigest()
                         if templist:
-                              for i in range(len(templist)):
-                                    item = templist[i]
-                                    if file not in item:
-                                          tempdict['flag'] = 'new'
-                                    elif tempdict['md5'] != t.hexdigest():
-                                          tempdict['flag'] = 'modify'
-                                          tempdict['md5'] = t.hexdigest()
-                                    else:
-                                          pass
-                              templist[i] = tempdict
+                              if file in map(lambda x:x['filename'], templist):
+                                    for i in range(len(templist)):
+                                          if file == templist[i]['filename'] and t.hexdigest() != templist[i]['filename']:
+                                                templist[i]['md5'] = t.hexdigest()
+                                                templist[i]['flag'] = 'modify'
+                              else:
+                                    tempdict['filename'] = file
+                                    tempdict['flag'] = 'new'
+                                    tempdict['md5'] = t.hexdigest()
+                                    templist.append(tempdict)
                         else:
+                              tempdict['filename'] = file
                               tempdict['flag'] = 'new'
+                              tempdict['md5'] = t.hexdigest()
                               templist.append(tempdict)
-
-            pickle.dump(templist, confighandler)
-            return templist
+            with open(config, 'wb') as confighandler:
+                  pickle.dump(templist, confighandler)
+            return templist, len(templist)
       except Exception, e:
             print e
-      finally:
-            confighandler.close()
 
 
 if __name__ == '__main__':
       # path = raw_input('Please enter a path:\n')
       path = '/home/caicai/Dev/Dev/my_blog'
+      # path = u'E:\kankan'
       if os.path.exists(path):
             print createmd5(path)
       else:
